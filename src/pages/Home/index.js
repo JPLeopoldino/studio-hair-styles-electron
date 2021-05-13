83449fba2c90d96dedaf7b8ea620762357ca889a
JPLeopolino <leopoldino26@gmail.com>
2021-05-13 20:43:07 -0300

    feat: Começando a tela Home/Dashboard

---
+++ src/pages/Home/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import * as SC from './styles';
+
+const Home = () => {
+    return(
+        <SC.Container>
+            <SC.Title>Teste</SC.Title>
+            <p>Testando o app</p>
+        </SC.Container>
+    );
+}
+
+export default Home;
