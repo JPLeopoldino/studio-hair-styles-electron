7c91513d7ecc03dce290f6690446af8e4cc6e236
NailtonBrito <nailjoga2002@gmail.com>
2021-05-27 19:29:06 -0300

    Login "Restantes"

--- src/app.js
+++ src/app.js
@@ -3,12 +3,14 @@ import React from 'react';
 import ProfessionalProfile from './pages/ProfessionalProfile';
 import ServicesHistory from './pages/ServicesHistory';
 import EditClient from './pages/EditClient';
+import Login from './pages/Login';
 
 const App = () =>{
     return (
+      <Login />
       /* <ProfessionalProfile /> */
       /* <ServicesHistory/> */
-      <EditClient/>
+      /*<EditClient/>*/
     );
 }
 
