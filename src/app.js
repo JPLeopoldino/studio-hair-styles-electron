9fcd4c7f7f2af14849883cea71e796c6269c3a2b
Kely <kelly_le_23mai@hotmail.com>
2021-05-27 19:45:57 -0300

    DataTable no SearchClient

--- src/app.js
+++ src/app.js
@@ -3,12 +3,14 @@ import React from 'react';
 import ProfessionalProfile from './pages/ProfessionalProfile';
 import ServicesHistory from './pages/ServicesHistory';
 import EditClient from './pages/EditClient';
+import SearchClient from './pages/SearchClient';
 
 const App = () =>{
     return (
-      <ProfessionalProfile />
+       <ProfessionalProfile />
       /* <ServicesHistory/> */
       /* <EditClient/> */
+      // <SearchClient />
     );
 }
 
