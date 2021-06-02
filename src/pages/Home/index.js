0bba6e95d7230f7950e9921bf2d511db35d04830
JPLeopolino <leopoldino26@gmail.com>
2021-06-01 21:08:46 -0300

    feat: added antd lib

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import * as SC from './styles';
 import NavMenu from '../../components/NavMenu';
+// import { InfoTile, ChatBox } from 'adminlte-reactjs';
+import { Button, Calendar } from 'antd';
 
 const Home = () => {
     return(
         <SC.MainContainer>
             <NavMenu/>
             <SC.Container>
+                <Button type="primary">Primary Button</Button>
+                <Calendar/>
                 <SC.Title>Teste</SC.Title>
                 <p>Testando o app</p>
             </SC.Container>
