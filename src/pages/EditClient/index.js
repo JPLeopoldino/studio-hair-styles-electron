30f7a9cb2c10b7e11ca0e8a826bec259f2f4b90a
Kely <kelly_le_23mai@hotmail.com>
2021-06-15 21:04:21 -0300

    Commit tela editar cliente

--- src/pages/EditClient/index.js
+++ src/pages/EditClient/index.js
@@ -1,13 +1,60 @@
 import React from 'react';
-import NavMenu from '../../components/NavMenu';
+import { Container, FormDiv, InputStyles, Button, TitleStyle } from './styles';
+import {Link} from 'react-router-dom';
+import {useFormik} from 'formik';
+import * as Yup from 'yup';
 
-const EditClient = () => {
+const EditClient = () =>{
+
+    const formik = useFormik({
+        initialValues: {
+            name: '', email: '',
+            phone: '',
+            birthDate: '',
+        },
+        validationSchema: Yup.object({
+            email: Yup.string().required('O email é obrigatorio').email('Email invalido'),            
+        })
+    });
+    
     return(
         <div>
-            <NavMenu />
-            <h1>Editar cliente</h1>
+            <Container>
+                <TitleStyle>Editar Cliente</TitleStyle>
+
+                <FormDiv onSubmit={formik.handleSubmit}>
+                    <InputStyles
+                        type="text" name="name" value={formik.values.name}
+                        onChange={formik.handleChange} placeholder="Nome"
+                    />
+
+                    <InputStyles 
+                        type="email" name="email" value={formik.values.email}
+                        onChange={formik.handleChange} placeholder="Email"
+                    />                    
+
+                    <InputStyles
+                        type="number" name="phone" value={formik.values.phone}
+                        onChange={formik.handleChange} placeholder="Telefone"
+                    />
+
+                    <InputStyles
+                        type="text" name="birthDate" value={formik.values.birthDate}
+                        onChange={formik.handleChange} placeholder="Data de Nascimento"
+                    />
+
+                    <br/><Button type="submit">ENVIAR</Button>
+
+                    <Link to="/searchClient">
+                        <Button>VOLTAR</Button>
+                    </Link>                    
+                    
+                </FormDiv>
+
+                
+            </Container>
         </div>
-    )
+    );
 }
 
 export default EditClient;
