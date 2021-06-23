6cc2fd6e62232afb24c2457237e9f149b88ed479
JPLeopolino <leopoldino26@gmail.com>
2021-06-23 18:56:49 -0300

    Editando a tela Home

--- src/pages/Home/styles.js
+++ src/pages/Home/styles.js
@@ -1,4 +1,5 @@
 import styled from 'styled-components';
+import colors from '../../styles/colors';
 
 export const MainContainer = styled.div`
     display: flex;
@@ -9,13 +10,40 @@ export const Container = styled.div`
     display: flex;
     flex-direction: column;
     align-items: center;
-    justify-content: space-evenly;
-    background-color: pink;
+    justify-content: flex-start;
     height: 100vh;
     width: 100vw;
 `;
 
+export const ImageContainer = styled.div`
+    width: 200px;
+    height: 200px;
+    overflow: hidden;
+    border-radius: 100%;
+    border: 4px solid #7B206F;
+    margin-top: 80px;
+    margin-bottom: 30px;
+`
+
+export const Image = styled.img`
+    width: 100%;
+    height: auto;
+`
+
+export const TitleContainer = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+`
+
 export const Title = styled.h1`
     font-size: 34px;
     font-weight: bold;
+    margin-right: 5px;
 `;
+
+export const TitleUsername = styled.h1`
+    font-size: 48px;
+    font-weight: bold;
+    text-transform: capitalize;
+`
